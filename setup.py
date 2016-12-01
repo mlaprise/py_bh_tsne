3b19c313d26d9046a2864ce627099a77669b7e65
Martin Laprise <mlaprise@gmail.com>
2016-12-01 16:21:04 -0500

    clean the setup

--- setup.py
+++ setup.py
@@ -10,26 +10,22 @@ import sys
 if sys.platform == 'darwin':
     ext_modules = [Extension(
         name="fasttsne",
-        sources=["fasttsne/orig-lvdm/quadtree.cpp",
-                 "fasttsne/orig-lvdm/tsne.cpp", "fasttsne/fasttsne.pyx"],
-        include_dirs=[numpy.get_include(), "fasttsne/orig-lvdm/"],
-        extra_compile_args=[
-            '-faltivec', '-I/System/Library/Frameworks/vecLib.framework/Headers'],
+        sources=["fasttsne/orig-lvdm/quadtree.cpp", "fasttsne/orig-lvdm/tsne.cpp", "fasttsne/fasttsne.pyx"],
+        include_dirs=[numpy.get_include(),
+                      "fasttsne/orig-lvdm/",
+                      "/System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/Headers"],
+        #extra_compile_args=['-faltivec', '-I/System/Library/Frameworks/vecLib.framework/Headers'],
         extra_link_args=["-Wl,-framework", "-Wl,Accelerate", "-lcblas"],
         language="c++"
     )]
 else:
     ext_modules = [Extension(
         name="fasttsne.fasttsne",
-        sources=["fasttsne/orig-lvdm/quadtree.cpp",
-                 "fasttsne/orig-lvdm/tsne.cpp", "fasttsne/fasttsne.pyx"],
-        include_dirs=[numpy.get_include(),
-                      "/home/dmitry/miniconda/pkgs/openblas-0.2.14-1/include",
-                       "/usr/local/include", "fasttsne/orig-lvdm/"],
+        sources=["fasttsne/orig-lvdm/quadtree.cpp", "fasttsne/orig-lvdm/tsne.cpp", "fasttsne/fasttsne.pyx"],
+        include_dirs=[numpy.get_include(), "/usr/local/include", "fasttsne/orig-lvdm/"],
         library_dirs=["/usr/local/lib"],
         extra_compile_args=['-msse2', '-O3', '-fPIC', '-w'],
         extra_link_args=["-lopenblas"],
-       # data_files=["fasttsne/__init__.py"],
         language="c++"
     )]
 
